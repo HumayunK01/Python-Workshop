594e0076bace5d0c2bad6d8c3e197539cbcfa629
Humayun Khan <humayunk.pvt@gmail.com>
2023-03-04 21:30:21 +0530

    init

---
+++ Day1.py
@@ -0,0 +1,35 @@
+# Data Types
+# String ----> Alphabet ----> Humayun Khan
+# Integer ----> Numeric ----> 10, 20, 30, 200, etc
+# Float ----> Decimal ----> 10.4, 59.3, etc
+# Boolean ----> Bool ----> True, False
+# Complex Numbers ----> Complex ----> a+ib, i+iy
+
+# Jupyter, VS Code, Pycharm, Spyder, JupterLab, Google Colab, etc
+
+# Conversion of the data...
+y = 50.6
+type(y)
+
+# How to convert data type:
+z = int(y)  # Converting the datatype and storing it in another variable
+type(z)
+print(z)
+
+# Conditional Statement
+a = 10
+if a > 5:
+    print("It is greater than")
+else:
+    print("Less than value")
+
+# Loops in Python
+# For Loop:
+# To run something again and again (In general)
+# Syntax: for value in values:
+#            Output
+
+for i in "Humayun":
+    print(i)
+for x in "EdYoda University":
+    print(x)
